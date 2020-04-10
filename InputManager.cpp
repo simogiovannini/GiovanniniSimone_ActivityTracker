4c1ecc5e7c87061096fb89e3708e8dc6fc5e3027
simone <esseg99.simo@gmail.com>
2020-04-10 23:17:52 +0200

    Creazione classe InputManager

---
+++ InputManager.cpp
@@ -0,0 +1,20 @@
+//
+// Created by simone on 10/04/20.
+//
+
+#include <iostream>
+#include "InputManager.h"
+
+int InputManager::getNumber(int min, int max) {
+    int value;
+    while (true) {
+        std::cin >> value;
+
+        if (value < min || value > max) {
+            std::cout << "Devi inserire un valore compreso tra " << min << " e " << max << "!" << std::endl;
+        }
+
+        break;
+    }
+    return value;
+}
---
+++ InputManager.h
@@ -0,0 +1,15 @@
+//
+// Created by simone on 10/04/20.
+//
+
+#ifndef ACTIVITYTRACKER_INPUTMANAGER_H
+#define ACTIVITYTRACKER_INPUTMANAGER_H
+
+
+class InputManager {
+public:
+    static int getNumber(int min, int max);
+};
+
+
+#endif //ACTIVITYTRACKER_INPUTMANAGER_H
